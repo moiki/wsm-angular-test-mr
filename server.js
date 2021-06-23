c21d7b7c03dc146f8bffd77cf9b3ef26bb30b671
moiki <moiki.reyes@gmail.com>
2021-06-23 03:05:18 -0600

    fixing deploy

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const express = require("express");
 const app = express();
 
 app.use(requireHTTPS);
-app.use(express.static("./dist/angular-heroku"));
+app.use(express.static("./dist/wsm-angular-test-mr"));
 
 app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "dist/angular-heroku/" })
+  res.sendFile("index.html", { root: "dist/wsm-angular-test-mr/" })
 );
 
 app.listen(process.env.PORT || 8080);
